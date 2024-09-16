9f610156d9ba708096144cc448baed3e890975b0
1663106 <1663106@TE-GDA-D-D21E07>
2024-09-16 11:08:53 +0200

    Dodaj pliki projektów.

---
+++ OrginalnaNazwaKostka/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace OrginalnaNazwaKostka
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ OrginalnaNazwaKostka/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace OrginalnaNazwaKostka
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
