ba0a15b2c9ee2ffa86f4f650852c8380b8b41e5d
Kluczleks <kluczleks.twitch@gmail.comn>
2024-09-16 12:29:58 +0200

    To jest na 4

--- OrginalnaNazwaKostka/MainPage.xaml.cs
+++ OrginalnaNazwaKostka/MainPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿
+using System.Security.Cryptography.X509Certificates;
 
 namespace OrginalnaNazwaKostka
 {
@@ -13,13 +14,46 @@ namespace OrginalnaNazwaKostka
 
         public void OnCounterClicked(object sender, EventArgs e)
         {
-            Random rand = new Random();
-            int randomNumber = rand.Next(1, 6);
-            kostka.Text = randomNumber.ToString()+ " to twój wynik";
-            SemanticScreenReader.Announce(kostka.Text); 
-            // komenda taka 
+
+            if (K4.IsChecked)
+            {
+                Random rand = new Random();
+                int randomNumber = rand.Next(1, 5);
+                kostka.Text = randomNumber.ToString() + " to twój wynik";
+                    
+            }
+            if (K6.IsChecked) 
+            {
+                Random rand = new Random();
+                int randomNumber = rand.Next(1, 7);
+                kostka.Text = randomNumber.ToString() + " to twój wynik";
+               
+            }
+            if (K10.IsChecked) 
+            {
+                Random rand = new Random();
+                int randomNumber = rand.Next(1, 11);
+                kostka.Text = randomNumber.ToString() + " to twój wynik";
+                
+            }
+            if (K12.IsChecked) 
+            {   
+                Random rand = new Random();
+                int randomNumber = rand.Next(1, 13);
+                kostka.Text = randomNumber.ToString() + " to twój wynik";
+               
+            }
+
+
+
+
+
+            // kostka.Text = randomNumber.ToString() + " to twój wynik";
+            //SemanticScreenReader.Announce(kostka.Text);
+            // komenda taka
+
         }
     }
-  
+
 
 }
